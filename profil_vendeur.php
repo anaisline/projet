d6aed8a4115f61272cdb1bf2bf195781c1ac2b1a
Anaïs Liné <anaisline22@gmail.com>
2021-12-07 21:46:22 +0100

    modif profil en cours

--- profil_vendeur.php
+++ profil_vendeur.php
@@ -30,7 +30,7 @@ $db_found = mysqli_select_db($db_handle, $database);
 				</font>
 			</p>
 		</div>
-<br>
+		<br>
 		<div id="nav">
 			<ul>
 				<li><a href="accueilVendeur.php">Accueil</a></li>
@@ -56,85 +56,95 @@ $db_found = mysqli_select_db($db_handle, $database);
 		</div>
 
 
-				<?php 
-				$sql = "SELECT * from vendeur WHERE (id_vendeur = '$id_vendeur')";
-				$result = mysqli_query($db_handle, $sql);
+		<?php 
+		$sql = "SELECT * from vendeur WHERE (id_vendeur = '$id_vendeur')";
+		$result = mysqli_query($db_handle, $sql);
 
 
-				$data = mysqli_fetch_assoc($result);
-				$image = $data['photo'];
-				?>
+		$data = mysqli_fetch_assoc($result);
+		$image = $data['photo'];
+		?>
 
 		<div class="container rounded bg-black mt-8 mb-8">
+			<form action="verifModifProfil.php" method="post">
 			<div class="row">
 				<div class="col-md-5 ">
 					<div class="d-flex flex-column align-items-center text-center p-3 py-5">
-						<img class="rounded-circle mt-5" width="150px" src= "<?php echo $image; ?>"><span class="font-weight-bold">Edogaru</span><span class="text-black-50">edogaru@mail.com.my</span><span> </span></div>
-				</div>
-				<div class="col-md-5 ">
-					<div class="p-3 py-5">
-						<div class="d-flex justify-content-between align-items-center mb-3">
-							<h4 class="text-right">Paramètres du profil</h4>
+						<img class="rounded-circle mt-5" width="150px" src= "<?php echo $image; ?>"><span class="font-weight-bold"><?php echo $data['prenom']." ".$data['nom']; ?></span><span class="text-black-50"><?php echo $data['description']; ?></span><span> </span></div>
+					</div>
+					<div class="col-md-5 ">
+						<div class="p-3 py-5">
+							<div class="d-flex justify-content-between align-items-center mb-3">
+								<h4 class="text-right">Paramètres du profil</h4>
+							</div>
+							<div class="row mt-2">
+								<div class="col-md-6"><label class="labels">Prénom</label><input type="text" class="form-control"  name="prenom" value="<?php echo $data['prenom']; ?>"></div>
+
+								<div class="col-md-6"><label class="labels">Nom</label><input type="text" class="form-control"  name="nom" value="<?php echo $data['nom']; ?>"></div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-md-6"><label class="labels">URL photo de profil</label><input type="text" class="form-control"  name="photo" value="<?php echo $data['photo']; ?>"></div>
+
+								<div class="col-md-6"><label class="labels">Téléphone</label><input type="text" class="form-control"  name="tel" value="<?php echo $data['tel']; ?>"></div>
+							</div>
+							<div class="row mt-2">
+								<div class="col-md-6"><label class="labels">Mail</label><input type="text" class="form-control"  name="mail" value="<?php echo $data['mail']; ?>"></div>
+
+								<div class="col-md-6"><label class="labels">Mot de passe</label><input type="text" class="form-control"  name="mdp" value="<?php echo $data['mdp']; ?>"></div>
+							</div>
+							<div class="row mt-3">
+								<div class="col-md-12"><label class="labels">Description</label>
+									<input type="text" class="form-control" name="description" value="<?php echo $data['description']; ?>">
+								</div>
+							</div>
+							<div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="submit" name="bouton1">Enregistrer les modifications</button></div>
 						</div>
-						<div class="row mt-2">
-							<div class="col-md-6"><label class="labels">Prénom</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-							<div class="col-md-6"><label class="labels">Nom</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-						</div>
-						<div class="row mt-2">
-							<div class="col-md-6"><label class="labels">URL photo de profil</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-							<div class="col-md-6"><label class="labels">Téléphone</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-						</div>
-						<div class="row mt-2">
-							<div class="col-md-6"><label class="labels">Mail</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-							<div class="col-md-6"><label class="labels">Mot de passe</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-						</div>
-						 <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">Description</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
-						</div>
-						<div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
 					</div>
 				</div>
 			</div>
+		</form>
 		</div>
 
 
-		<div id="footer">
-			<!--Copyright &copy; 2021 Prime Properties<br> -->
 
-			<dd>
-				<ul>
-					<li>
-						<p align=left>
-							Qui sommes-nous ?<br><br>
-							Nous sommes un groupe d'étudiants qui avons<br> pour but de créer un site internet pour vous permettre de<br> faire votre shopping facilement.<br>
-							Nous avons réfléchi à ce dont vous pourriez avoir<br> besoin et il nous est apparu que nous avons beaucoup <br>de difficultés à faire hanter les personnes qui le méritent...<br>
-							<p align=right>
-								Alors nous voila !
+
+			<div id="footer">
+				<!--Copyright &copy; 2021 Prime Properties<br> -->
+
+				<dd>
+					<ul>
+						<li>
+							<p align=left>
+								Qui sommes-nous ?<br><br>
+								Nous sommes un groupe d'étudiants qui avons<br> pour but de créer un site internet pour vous permettre de<br> faire votre shopping facilement.<br>
+								Nous avons réfléchi à ce dont vous pourriez avoir<br> besoin et il nous est apparu que nous avons beaucoup <br>de difficultés à faire hanter les personnes qui le méritent...<br>
+								<p align=right>
+									Alors nous voila !
+								</p>
 							</p>
-						</p>
-					</li>
-					<li>
-						<p class="contact">Nous contacter: <br><br>
-							<a href="mailto:paris.shopping@gmail.com">paris.shopping@gmail.com</a><br>
-							67 avenue Henri Martin 75016 PARIS<br>
-							02 37 60 03 10<br>
+						</li>
+						<li>
+							<p class="contact">Nous contacter: <br><br>
+								<a href="mailto:paris.shopping@gmail.com">paris.shopping@gmail.com</a><br>
+								67 avenue Henri Martin 75016 PARIS<br>
+								02 37 60 03 10<br>
 
-						</p>
+							</p>
 
-					</li>
-					<li>
-						<p>
-							Instagram des auteurs :<br>
-							<br> <a href="https://www.instagram.com/anaisline_/" class="button">> anaisline_</a>
-							<br> <a href="https://www.instagram.com/marine_rhd/" class="button">> marine_rhd</a>
-							<br> <a href="https://www.instagram.com/benji.lvld/" class="button">> benji.lvld</a>
-						</p>
-					</li>
-				</ul>
-			</dd>
+						</li>
+						<li>
+							<p>
+								Instagram des auteurs :<br>
+								<br> <a href="https://www.instagram.com/anaisline_/" class="button">> anaisline_</a>
+								<br> <a href="https://www.instagram.com/marine_rhd/" class="button">> marine_rhd</a>
+								<br> <a href="https://www.instagram.com/benji.lvld/" class="button">> benji.lvld</a>
+							</p>
+						</li>
+					</ul>
+				</dd>
+			</div>
+
 		</div>
 
-	</div>
-
-</body>
-</html>
+	</body>
+	</html>
--- verifModifProfil.php
+++ verifModifProfil.php
@@ -6,36 +6,88 @@ $photo=isset($_POST['photo'])?$_POST['photo']:"";
 $nom=isset($_POST['nom'])?$_POST['nom']:"";
 $prenom=isset($_POST['nom'])?$_POST['nom']:"";
 $description=isset($_POST['description'])?$_POST['description']:"";
+$mail=isset($_POST['mail'])?$_POST['mail']:"";
+$mdp=isset($_POST['mdp'])?$_POST['mdp']:"";
+$tel=isset($_POST['tel'])?$_POST['tel']:"";
 
 //identifier BDD
 $database = "shopping";
 //connectez-vous dans BDD
 $db_handle = mysqli_connect('localhost', 'root', '');
 $db_found = mysqli_select_db($db_handle, $database);
+$erreur=0;
+
+if (isset($_POST["bouton1"])){
 
 
+    if ($db_found) {
 
-if ($db_found) {
+        if($photo!="")
+        {
+            $sql="UPDATE vendeur SET photo='$photo' WHERE id_vendeur='$id_vendeur' ";
+        }
+        if($nom!="")
+        {
+            $sql="UPDATE vendeur SET nom='$nom' WHERE id_vendeur='$id_vendeur' ";
+        }
+        if($prenom!="")
+        {
+            $sql="UPDATE vendeur SET prenom='$prenom' WHERE id_vendeur='$id_vendeur' ";
+        }
+        if($description!="")
+        {
+            $sql="UPDATE vendeur SET description='$description' WHERE id_vendeur='$id_vendeur' ";
+        }
+        if($mdp!="")
+        {
+            $sql="UPDATE vendeur SET mdp='$mdp' WHERE id_vendeur='$id_vendeur' ";
+        }
+        if($tel!="")
+        {
+            $sql="UPDATE vendeur SET tel='$tel' WHERE id_vendeur='$id_vendeur' ";
+        }
+        if($mail!="")
+        {
+        $//on cherche si un compte avec cet email existe deja parmi les acheteurs
+        $sql = "SELECT * FROM acheteur WHERE mail LIKE '%$mail%'";
+        $resultAcheteur = mysqli_query($db_handle, $sql);
+        //regarder s'il y a de resultat
 
-    if($photo!="")
-    {
-        $sql="UPDATE vendeur SET photo='$photo' WHERE id_vendeur='$id_vendeur' ";
+        //on cherche si un compte avec cet email existe deja parmi les vendeurs
+        $sql = "SELECT * FROM vendeur WHERE mail LIKE '%$mail%'";
+        $resultVendeur = mysqli_query($db_handle, $sql);
+        //regarder s'il y a de resultat
+
+        //on cherche si un compte avec cet email existe deja parmi les admin
+        $sql = "SELECT * FROM administrateur WHERE mail LIKE '%$mail%'";
+        $resultAdmin = mysqli_query($db_handle, $sql);
+
+        //regarder s'il y a de resultat
+        if (mysqli_num_rows($resultAcheteur) != 0 || mysqli_num_rows($resultVendeur) != 0 || mysqli_num_rows($resultVendeur) != 0) {
+            $erreur=1;
+        }
+        else
+        {
+            $sql="UPDATE vendeur SET description='$description' WHERE id_vendeur='$id_vendeur' ";
+        }
+
     }
-    if($nom!="")
+    if($erreur==0)
     {
-        $sql="UPDATE vendeur SET nom='$nom' WHERE id_vendeur='$id_vendeur' ";
+        header('Location: profil_vendeur.php?erreur=0');
     }
-    if($prenom!="")
+    else
     {
-        $sql="UPDATE vendeur SET prenom='$prenom' WHERE id_vendeur='$id_vendeur' ";
+         header('Location: profil_vendeur.php?erreur=1');
     }
-    if($description!="")
-    {
-        $sql="UPDATE vendeur SET description='$description' WHERE id_vendeur='$id_vendeur' ";
-    }
-
-    header('Location: profil_vendeur.php?');
+    
+    
 }
+
+
+}
+
+
 
 
 
