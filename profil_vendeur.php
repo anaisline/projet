ba83ed1f1baeb1695baf69b437864e4ef8d62614
Anaïs Liné <anaisline22@gmail.com>
2021-12-07 15:28:07 +0100

    correction beug connecter

--- profil_vendeur.php
+++ profil_vendeur.php
@@ -1,84 +1,147 @@
+<?php
+session_start();
+$id_vendeur=$_SESSION['id_vendeur'];
+
+$database = "shopping";
+//connectez-vous dans BDD
+$db_handle = mysqli_connect('localhost', 'root', '');
+$db_found = mysqli_select_db($db_handle, $database);
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<link href="accueilVendeur.css" rel="stylesheet" type="text/css"/>
+	<link href="profil_vendeur.css" rel="stylesheet" type="text/css"/>
 	<title>Fray Her</title>
 </head>
 <body>
 
-<!-- wrapper area -->
-<div id="wrapper">
+	<!-- wrapper area -->
+	<div id="wrapper">
 
-	<div id="header">
+		<div id="header">
+			<p>
+				<font>
+					Fray Her
+				</font>
+			</p>
+		</div>
+
+		<div id="nav">
+			<ul>
+				<li><a href="accueilVendeur.php">Accueil</a></li>
+
+				<li >
+					<a href="parcourir.html">Gerer mes articles</a>
+				</li>
+
+
+				<li><a href="#">Messagerie</a></li>
+
+
+				<li class="menu-deroulant">
+					<a href="#">Mon compte</a>
+					<ul class="sous-menu">
+						<li><a href="#">Mon profil</a></li>
+						<li><a href="connexionAcheteur.php">Se deconnecter</a></li>
+					</ul>	
+				</li>
+
+			</ul>
+
+		</div>
+		<div>
+
+		
+			<h2>Profil</h2> 
+			<p>
+			<?php 
+			$sql = "SELECT * from vendeur WHERE (id_vendeur = '$id_vendeur')";
+			$result = mysqli_query($db_handle, $sql);
+
+
+			$data = mysqli_fetch_assoc($result);
+			$image = $data['photo'];
+
+			echo "Photo "."<br>" ;
+			if($data['photo']!="")
+			{
+				echo "<img src='$image' height='120' width='100'>" . "<br>";
+			}
+			else
+			{
+				echo "<img src='photos/avatar.jpg' height='120' width='100'>" . "<br>";
+			}
+			
+			echo "<th>" . "Nom : " ;
+			echo $data['nom'] . "<br>";
+			echo "<th>" . "Prenom : " ;
+			echo $data['prenom'] . "<br>";
+			echo "<th>" . "Description : " ;
+			echo $data['description'] . "<br>";
+
+?>
+<a href="parcourir.html">Modifier mes infos profil</a>		
+</p>
+		<br> <br>
+		<h2>Sécurité</h2>
 		<p>
-			<font>
-				Fray Her
-			</font>
+		<?php
+
+		$sql = "SELECT * from vendeur WHERE (id_vendeur = '$id_vendeur')";
+		$result = mysqli_query($db_handle, $sql);
+		$data = mysqli_fetch_assoc($result);
+
+		echo "Mail : " . $data['mail'] . "<br>";
+		echo "Mot de passe : " . $data['mdp'] . "<br>";
+		echo "Telephone : " . $data['tel'] . "<br>";
+
+		?>
+		<a href="modifSecu.php">Modifier mes infos sécurité</a>
 		</p>
- 	</div>
- 
- 	<div id="nav">
- 		<ul>
- 			<li><a href="accueilVendeur.html">Accueil</a></li>
-
- 			<li >
- 			<a href="parcourir.html">Gerer mes articles</a>
- 			</li>
-
- 			
- 			<li><a href="#">Messagerie</a></li>
 
 
- 			<li class="menu-deroulant">
- 			<a href="#">Mon compte</a>
- 			<ul class="sous-menu">
- 				<li><a href="#">Mon profil</a></li>
- 				<li><a href="#">Se deconnecter</a></li>
- 			</ul>	
- 			</li>
- 		
- 		</ul>
+	</div>
 
- 	</div>
 
- 	
- 
- 	<div id="footer">
- 		<!--Copyright &copy; 2021 Prime Properties<br> -->
 
- 		<dd>
- 			<ul>
- 				<li>
- 					<p align=left>
- 						Qui sommes-nous ?<br><br>
- 						Nous sommes un groupe d'étudiants qui avons<br> pour but de créer un site internet pour vous permettre de<br> faire votre shopping facilement.<br>
- 						Nous avons réfléchi à ce dont vous pourriez avoir<br> besoin et il nous est apparu que nous avons beaucoup <br>de difficultés à faire hanter les personnes qui le méritent...<br>
- 						<p align=right>
- 							Alors nous voila !
- 						</p>
- 					</p>
- 				</li>
- 				<li>
- 					<p class="contact">Nous contacter: <br><br>
- 						<a href="mailto:paris.shopping@gmail.com">paris.shopping@gmail.com</a><br>
- 						67 avenue Henri Martin 75016 PARIS<br>
- 						02 37 60 03 10<br>
+	<div id="footer">
+		<!--Copyright &copy; 2021 Prime Properties<br> -->
 
- 					</p>
+		<dd>
+			<ul>
+				<li>
+					<p align=left>
+						Qui sommes-nous ?<br><br>
+						Nous sommes un groupe d'étudiants qui avons<br> pour but de créer un site internet pour vous permettre de<br> faire votre shopping facilement.<br>
+						Nous avons réfléchi à ce dont vous pourriez avoir<br> besoin et il nous est apparu que nous avons beaucoup <br>de difficultés à faire hanter les personnes qui le méritent...<br>
+						<p align=right>
+							Alors nous voila !
+						</p>
+					</p>
+				</li>
+				<li>
+					<p class="contact">Nous contacter: <br><br>
+						<a href="mailto:paris.shopping@gmail.com">paris.shopping@gmail.com</a><br>
+						67 avenue Henri Martin 75016 PARIS<br>
+						02 37 60 03 10<br>
 
- 				</li>
- 				<li>
- 					<p>
- 						Instagram des auteurs :<br>
- 						<br> <a href="https://www.instagram.com/anaisline_/" class="button">> anaisline_</a>
- 						<br> <a href="https://www.instagram.com/marine_rhd/" class="button">> marine_rhd</a>
- 						<br> <a href="https://www.instagram.com/benji.lvld/" class="button">> benji.lvld</a>
- 					</p>
- 				</li>
- 			</ul>
- 		</dd>
- 	</div>
+					</p>
+
+				</li>
+				<li>
+					<p>
+						Instagram des auteurs :<br>
+						<br> <a href="https://www.instagram.com/anaisline_/" class="button">> anaisline_</a>
+						<br> <a href="https://www.instagram.com/marine_rhd/" class="button">> marine_rhd</a>
+						<br> <a href="https://www.instagram.com/benji.lvld/" class="button">> benji.lvld</a>
+					</p>
+				</li>
+			</ul>
+		</dd>
+	</div>
 
 </div>
 
