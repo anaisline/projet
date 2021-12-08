e271eadef24b65052674a8558a5b02f859781ef6
marine-richard <78956452+marine-richard@users.noreply.github.com>
2021-12-08 17:00:53 +0100

    Supprimer ajout suppression photo

--- supprimerArticlesVendeur_2.php
+++ supprimerArticlesVendeur_2.php
@@ -39,27 +39,38 @@ $id_vendeur=$_SESSION['id_vendeur'];
 							$sql .= " WHERE nom LIKE '%$nom%'";
 						}
 						$resultArticle = mysqli_query($db_handle, $sql);
+						$data=mysqli_fetch_assoc($resultArticle);
 						//regarder s'il y a de resultat
 
 						if (mysqli_num_rows($resultArticle) != 0 ) {
-							echo "<p>Vous possedez deja un article qui possede ce nom donc on  peut le supprimer</p>";
-							echo "nom ".$nom;
-							echo "categorie achat ".$categorie_achat;
-							echo "categorie type ".$categorie_type;
+							echo "<p>Vous possedez un article qui possede ce nom donc on peut le supprimer</p>";
+//il faut recuperer l id de l article pour pouvoir supprimer les photos associees
+							$id_article=$data['id_article'];
+
+
+							$sql= "DELETE FROM photo";
+							if ($id_article != " ") {
+								$sql .= " WHERE id_article LIKE '%$id_article%'";	
+
+							}
+							$result =mysqli_query($db_handle, $sql);
+							echo "<p>Suppression de la photo de l article successfull.</p>";
+
+							
 							$sql= "DELETE FROM article_vendeur";
 							if ($nom != " ") {
 								$sql .= " WHERE nom LIKE '%$nom%'";
+							
 								if ($categorie_type != " ") {
-									$sql .= " AND categorie_type LIKE '%$categorie_type%'";   
+									$sql .= " AND categorie_type LIKE '%$categorie_type%'";
+									  
 									if($categorie_achat != " "){
 										$sql .= " AND categorie_achat LIKE '%$categorie_achat%'";
-										if($id_vendeur != ""){
-											$sql .= "AND id_vendeur LIKE '%$id_vendeur%'";
-										}
-
+										
 									}
 								}
 							}
+							
 							$result =mysqli_query($db_handle, $sql);
 							echo "<p>Suppression de l article successfull.</p>";
 
