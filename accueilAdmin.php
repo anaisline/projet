aa197b042a9f09ab4f8e8c9d22165c833b707335
marine-richard <78956452+marine-richard@users.noreply.github.com>
2021-12-08 12:29:50 +0100

    lien entre pages

--- accueilAdmin.php
+++ accueilAdmin.php
@@ -32,7 +32,7 @@
  			<a href="parcourir.html">Gérer</a>
  			<ul class="sous-menu">
  				<li><a href="GestionVendeurAdmin.php">Les vendeurs</a></li>
- 				<li><a href="#">Mes articles</a></li>
+ 				<li><a href="gererArticlesAdmin.php">Mes articles</a></li>
  			</ul>
  			</li>
 
