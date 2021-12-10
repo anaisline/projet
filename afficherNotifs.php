225c492bb49e89545ff568e9fa0af1f6ae47c69e
Anaïs Liné <anaisline22@gmail.com>
2021-12-10 15:25:30 +0100

    AVANCEE NOTIFS MAIS PB

--- afficherNotifs.php
+++ afficherNotifs.php
@@ -2,10 +2,7 @@
 session_start();
 $id_acheteur=$_SESSION['id_acheteur'];
 
-$database = "shopping";
-//connectez-vous dans BDD
-$db_handle = mysqli_connect('localhost', 'root', '');
-$db_found = mysqli_select_db($db_handle, $database);
+
 
 
 ?>
@@ -61,127 +58,241 @@ $db_found = mysqli_select_db($db_handle, $database);
 
 		</div>
 
-		<div id="section" align=center>
+		
+		<?php
+		$mysqli = new mysqli("localhost", "root", "", "shopping");
+		$mysqli -> set_charset("utf8");
+
+		$sql = "SELECT * from alerte WHERE id_acheteur='$id_acheteur'";
+		$results = $mysqli -> query($sql); 
+		$data = mysqli_fetch_assoc($results);
+		if($data==0)
+		{
+			?>
+			<div id="section" align=center>
+				<?php
+				echo "Vous n'avez pas créé d'alerte." ;
+				?>
+			</div>
 			<?php
 
-			$sql = "SELECT * from alerte WHERE id_acheteur='$id_acheteur'";
-			$results = mysqli_query($db_handle, $sql); 
-			$data = mysqli_fetch_assoc($results);
-			if ($data != 0 ) {
+		}
+		else if($data!=0) {
 
 				//affichage des articles trouvés selon les alertes
 			
 					///ALERTE n°i : on recupere les infos et on affiche l'alerte
-				$mot_cle_1 = $data['mot_cle_1'];
-				$mot_cle_2 = $data['mot_cle_2'];
-				$mot_cle_3 = $data['mot_cle_3'];
-				$categorie_type = $data['categorie_type'];
-				$categorie_achat = $data['categorie_achat'];
+			$mot_cle_1 = $data['mot_cle_1'];
+			$mot_cle_2 = $data['mot_cle_2'];
+			$mot_cle_3 = $data['mot_cle_3'];
+			$categorie_type = $data['categorie_type'];
+			$categorie_achat = $data['categorie_achat'];
 
 					///ON TROUVE LES ARTICLES VENDEUR AVEC LES MEMES INFOS
-				if($mot_cle_2!="" && $mot_cle_3!="")
+			if($mot_cle_2!="" && $mot_cle_3!="")
 				{ echo "cc1";
-					$data2=0;
-					$data21=0;
-					$data3=0;
-					$data31=0;
+			$data2=0;
+			$data21=0;
+			$data3=0;
+			$data31=0;
 
-					$sql1= "SELECT * FROM article_vendeur WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(description LIKE '%$mot_cle_2%')OR(description LIKE '%$mot_cle_3%')OR(nom LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_2%')OR(nom LIKE '%$mot_cle_3%'))";
-					$results1 = mysqli_query($db_handle, $sql1);
-					$data1 = mysqli_fetch_assoc($results1);
+			$sql1= "SELECT * FROM article_vendeur WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(description LIKE '%$mot_cle_2%')OR(description LIKE '%$mot_cle_3%')OR(nom LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_2%')OR(nom LIKE '%$mot_cle_3%'))";
+			$results1 = $mysqli -> query( $sql1);
+			$data1 = mysqli_fetch_assoc($results1);
 
-					$sql11= "SELECT * FROM article_admin WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(description LIKE '%$mot_cle_2%')OR(description LIKE '%$mot_cle_3%')OR(nom LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_2%')OR(nom LIKE '%$mot_cle_3%'))";
-					$results11 = mysqli_query($db_handle, $sql11);
-					$data11 = mysqli_fetch_assoc($results11);
-				}
-				else
-				{ 
-					$data1=0;
-					$data11=0;
+			$sql11= "SELECT * FROM article_admin WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(description LIKE '%$mot_cle_2%')OR(description LIKE '%$mot_cle_3%')OR(nom LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_2%')OR(nom LIKE '%$mot_cle_3%'))";
+			$results11 = $mysqli -> query($sql11);
+			$data11 = mysqli_fetch_assoc($results11);
+		}
+		else
+		{ 
+			$data1=0;
+			$data11=0;
 
-					if($mot_cle_2!="" && $mot_cle_3=="")
-					{
-						$data3=0;
-						$data31=0;
-
-						$sql2= "SELECT * FROM article_vendeur WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(description LIKE '%$mot_cle_2%')OR(nom LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_2%'))";
-						$results2 = mysqli_query($db_handle, $sql2);
-						$data2 = mysqli_fetch_assoc($results2);
-						
-						$sql21= "SELECT * FROM article_admin WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(description LIKE '%$mot_cle_2%')OR(nom LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_2%'))";
-						$results21 = mysqli_query($db_handle, $sql21);
-						$data21 = mysqli_fetch_assoc($results21);
-					}
-					else
-					{
-						$data2=0;
-						$data21=0;
-
-						$sql3= "SELECT * FROM article_vendeur WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_1%'))";
-						$results3 = mysqli_query($db_handle, $sql3);
-						$data3 = mysqli_fetch_assoc($results3);
-
-						$sql31= "SELECT * FROM article_admin WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_1%'))";
-						$results31 = mysqli_query($db_handle, $sql31);
-						$data31 = mysqli_fetch_assoc($results31);
-					}
-				}
-				
-				if($data1==0 && $data2==0 && $data3==0 && $data11==0 && $data21==0 && $data31==0)
-				{
-					echo "Vous n'avez pas de notification" ;
-				}
-
-
-				
-				
-			}
-			else //pas d'alerte encore créées 
+			if($mot_cle_2!="" && $mot_cle_3=="")
 			{
-				//echo "<td>" . "<img src="messagerie.png" height='120' width='100'>" . "</td>";
-				echo "Vous n'avez pas créé d'alerte." ;
+				$data3=0;
+				$data31=0;
+
+				$sql2= "SELECT * FROM article_vendeur WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(description LIKE '%$mot_cle_2%')OR(nom LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_2%'))";
+				$results2 = $mysqli -> query($sql2);
+				$data2 = mysqli_fetch_assoc($results2);
+
+				$sql21= "SELECT * FROM article_admin WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(description LIKE '%$mot_cle_2%')OR(nom LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_2%'))";
+				$results21 = $mysqli -> query( $sql21);
+				$data21 = mysqli_fetch_assoc($results21);
 			}
+			else
+			{
+				$data2=0;
+				$data21=0;
 
+				$sql3= "SELECT * FROM article_vendeur WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_1%'))";
+				$results3 = $mysqli -> query($sql3);
+				$data3 = mysqli_fetch_assoc($results3);
+
+				$sql31= "SELECT * FROM article_admin WHERE categorie_type='$categorie_type' and categorie_achat='$categorie_achat' and ((description LIKE '%$mot_cle_1%')OR(nom LIKE '%$mot_cle_1%'))";
+				$results31 = $mysqli -> query($sql31);
+				$data31 = mysqli_fetch_assoc($results31);
+			}
+		}
+
+		if($data1==0 && $data2==0 && $data3==0 && $data11==0 && $data21==0 && $data31==0)
+		{
 			?>
-		</div>
+			<div id="section" align=center>
+				<?php
+				echo "Vous n'avez pas de notification" ;
+				?>
+			</div>
+			<?php
 
-		<div id="footer">
-			<!--Copyright &copy; 2021 Prime Properties<br> -->
+		}
+		if($data1 != 0)
+		{
+			do{
+				?>
 
-			<dd>
-				<ul>
-					<li>
-						<p align=left>
-							Qui sommes-nous ?<br><br>
-							Nous sommes un groupe d'étudiants qui avons<br> pour but de créer un site internet pour vous permettre de<br> faire votre shopping facilement.<br>
-							Nous avons réfléchi à ce dont vous pourriez avoir<br> besoin et il nous est apparu que nous avons beaucoup <br>de difficultés à faire hanter les personnes qui le méritent...<br>
-							<p align=right>
-								Alors nous voila !
-							</p>
+
+
+				<form action="AjouterPanierAcheteurAdmin.php">
+					<table>
+						<tr align=center>
+							<td colspan='2'>
+								<?php
+								echo $data1['nom'];
+								?>
+							</td>
+						</tr>
+
+
+						<tr align=center>
+							<td>
+								<?php
+								$recup = $data1['id_article'];
+
+								$requPhoto = "SELECT adresse_photo from photo, article_vendeur where photo.id_article = '$recup'";
+								$resultatPhoto = $mysqli-> query($requPhoto);
+
+								$count = "SELECT count(id_photo) as numero from photo where photo.id_article = '$recup'";
+								$resCount = mysqli_query($mysqli, $count);
+								$data01 = mysqli_fetch_array($resCount);
+
+								for ($i=0; $i < $data01['numero']; $i++) { 
+									$ligne1 = $resultatPhoto -> fetch_assoc();
+									echo '<img src="'. $ligne1['adresse_photo'] .'" alt="" />';
+								}
+
+								?>
+							</td>
+							<td>
+								<?php
+								echo "Description : " . $data1['description'];
+								?>
+							</td>
+						</tr>
+						<?php
+
+						$recup = $data1['id_vendeur'];
+
+						$requNom = "SELECT * from vendeur where vendeur.id_vendeur = $recup";
+						$resultatNom = $mysqli -> query($requNom);
+
+						$data0 = $resultatNom -> fetch_assoc();
+						$var = $data0['nom'];
+						$var2 = $data0['prenom'];
+						?>
+
+						<tr align=center>
+							<td>
+								<?php
+								echo "<a href='VisiteProfilVend.php?nom=".$var."&prenom=".$var2." '>$var</a>";
+								?>
+							</td>
+							<td>
+								<?php
+								echo $data1['prix'] . " euros";
+								?>
+							</td>
+						</tr>
+
+						<tr align=center>
+							<td>
+								<?php
+								echo "Achat " . $data1['categorie_achat'];
+								?>
+							</td>
+							<td>
+								<?php
+								echo "Mis en vente le : " . $data1['date'];
+								?>
+							</td>
+						</tr>
+						<tr align=center>
+							<td colspan="2">
+								<?php
+								$var = $data1['id_article'];
+								echo "<a href='AjouterPanierAcheteurAdmin.php?id_article=".$var." '>Ajouter au panier</a>";
+								?>
+							</td>
+						</tr>
+
+					</table>
+				</form>
+
+
+
+
+
+				<?php
+				$data1 = $mysqli -> query($sql1);
+			}while($data1);
+		}//fin du if pour data 1
+
+
+
+	}//fin du gros if DERNIERE ACOLADE
+
+	?>
+
+
+	<div id="footer">
+		<!--Copyright &copy; 2021 Prime Properties<br> -->
+
+		<dd>
+			<ul>
+				<li>
+					<p align=left>
+						Qui sommes-nous ?<br><br>
+						Nous sommes un groupe d'étudiants qui avons<br> pour but de créer un site internet pour vous permettre de<br> faire votre shopping facilement.<br>
+						Nous avons réfléchi à ce dont vous pourriez avoir<br> besoin et il nous est apparu que nous avons beaucoup <br>de difficultés à faire hanter les personnes qui le méritent...<br>
+						<p align=right>
+							Alors nous voila !
 						</p>
-					</li>
-					<li>
-						<p class="contact">Nous contacter: <br><br>
-							<a href="mailto:paris.shopping@gmail.com">paris.shopping@gmail.com</a><br>
-							67 avenue Henri Martin 75016 PARIS<br>
-							02 37 60 03 10<br>
+					</p>
+				</li>
+				<li>
+					<p class="contact">Nous contacter: <br><br>
+						<a href="mailto:paris.shopping@gmail.com">paris.shopping@gmail.com</a><br>
+						67 avenue Henri Martin 75016 PARIS<br>
+						02 37 60 03 10<br>
 
-						</p>
+					</p>
 
-					</li>
-					<li>
-						<p>
-							Instagram des auteurs :<br>
-							<br> <a href="https://www.instagram.com/anaisline_/" class="button">> anaisline_</a>
-							<br> <a href="https://www.instagram.com/marine_rhd/" class="button">> marine_rhd</a>
-							<br> <a href="https://www.instagram.com/benji.lvld/" class="button">> benji.lvld</a>
-						</p>
-					</li>
-				</ul>
-			</dd>
-		</div>
-
+				</li>
+				<li>
+					<p>
+						Instagram des auteurs :<br>
+						<br> <a href="https://www.instagram.com/anaisline_/" class="button">> anaisline_</a>
+						<br> <a href="https://www.instagram.com/marine_rhd/" class="button">> marine_rhd</a>
+						<br> <a href="https://www.instagram.com/benji.lvld/" class="button">> benji.lvld</a>
+					</p>
+				</li>
+			</ul>
+		</dd>
 	</div>
+
+</div>
 
 </body>
 </html>
