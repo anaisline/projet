17f8516794bc305745b629cc9ea4691d26cccec2
marine-richard <78956452+marine-richard@users.noreply.github.com>
2021-12-10 11:17:06 +0100

    Achat immediat
    Achat a partir du panier

---
+++ achatImmediat.php
@@ -0,0 +1,211 @@
+<?php
+session_start();
+$id_acheteur=$_SESSION['id_acheteur'];
+
+$database = "shopping";
+//connectez-vous dans BDD
+$db_handle = mysqli_connect('localhost', 'root', '');
+$db_found = mysqli_select_db($db_handle, $database);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="nouveauClient.css" rel="stylesheet" type="text/css"/>
+	<title>Fray Her</title>
+</head>
+<body>
+	<div id="wrapper">
+
+		<div id="header">
+			<p>
+				<font>
+					Fray Her
+				</font>
+			</p>
+		</div>
+
+		<div id="nav">
+			<ul>
+				<li><a href="accueilAcheteur.html">Accueil</a></li>
+
+				<li class="menu-deroulant">
+					<a href="parcourir.html">Parcourir les categories</a>
+					<ul class="sous-menu">
+						<li><a href="#">Poupees</a></li>
+						<li><a href="#">Jeux</a></li>
+						<li><a href="#">Insolite</a></li>
+						<li><a href="#">Tout parcourir</a></li>
+					</ul>
+				</li>
+
+
+				<li><a href="#">Messagerie</a></li>
+
+				<li><a href="#">Panier</a></li>
+
+				<li class="menu-deroulant">
+					<a href="#">Mon compte</a>
+					<ul class="sous-menu">
+						<li><a href="profil_acheteur.php">Mon profil</a></li>
+						<li><a href="connexionAcheteur.php">Se deconnecter</a></li>
+					</ul>	
+				</li>
+
+			</ul>
+		</div>
+
+		<div id="section" align=center>
+			<form action="achatImmediat_2.php" method="post" align=center>
+				<table align=center>
+
+					<?php
+						$sql = "SELECT * from acheteur WHERE (id_acheteur = '$id_acheteur')";
+						$result = mysqli_query($db_handle, $sql);
+						$data = mysqli_fetch_assoc($result);
+					?>
+
+
+						<h2>Vos informations personnelles</h2>
+					<tr>
+						<td><label>Nom <span class="required"></span></label></td>
+						<td><input type="text" name="nom" class="field-long" value="<?php echo $data['nom']; ?>" />
+						</td>
+					</tr>
+
+					<tr>
+						<td><label>Prenom <span class="required"></span></label></td>
+						<td><input type="text" name="prenom" class="field-long" value="<?php echo $data['prenom']; ?>" />
+						</td>
+					</tr>
+
+				
+
+					<!--adresse est une table diff donc pas recup de la mm maniere-->
+					
+					<?php
+						$id_adresse=$data['id_adresse'];
+						$sqlA = "SELECT * from adresse WHERE (id_adresse = '$id_adresse')";
+						$resultAdresse = mysqli_query($db_handle, $sqlA);
+						$dataAdresse = mysqli_fetch_assoc($resultAdresse);
+					?>
+					<tr>
+						<td><label>Adresse l1 <span class="required"></span></label></td>
+						<td><input type="text" name="adresse_l1" class="field-long" value="<?php echo $dataAdresse['adresse_l1']; ?>" />
+						</td>
+					</tr>
+					<tr>
+						<td><label>Adresse l2 <span class="required"></span></label></td>
+						<td><input type="text" name="adresse_l2" class="field-long" value="<?php echo $dataAdresse['adresse_l2']; ?>" />
+						</td>
+					</tr>
+
+					<tr>
+					<td><label>Ville <span class="required"></span></label></td>
+					<td><input type="text" name="ville" class="field-long" value="<?php echo $dataAdresse['ville']; ?>" />
+					</td>
+					</tr>
+
+					<tr>
+					<td><label>Code postal <span class="required"></span></label></td>
+					<td><input type="text" name="code_postal" class="field-long" value="<?php echo $dataAdresse['code_postal']; ?>" />
+					</td>
+					</tr>
+
+					<tr>
+					<td><label>Telephone <span class="required"></span></label></td>
+					<td><input type="text" name="tel" class="field-long" value="<?php echo $data['tel']; ?>" />
+					</td>
+					</tr>
+
+					<h3>Vos informations bancaires</h3>
+
+					//cb est une table diff
+					<?php
+						$sqlCB = "SELECT * from cb WHERE (id_acheteur = '$id_acheteur')";
+						$resultCB = mysqli_query($db_handle, $sqlCB);
+						$dataCB = mysqli_fetch_assoc($resultCB);
+					?>
+					<tr>
+					<td><label>Type de carte <span class="required"></span></label></td>
+					<td><input type="text" name="type" class="field-long" value="<?php echo $dataCB['type']; ?>" />
+					</td>
+					</tr>
+
+					<tr>
+					<td><label>Numero de carte <span class="required"></span></label></td>
+					<td><input type="text" name="numero_cb" class="field-long" value="<?php echo $dataCB['numero_cb']; ?>" />
+					</td>
+					</tr>
+
+					<tr>
+					<td><label>Nom sur la carte <span class="required"></span></label></td>
+					<td><input type="text" name="nomCarte" class="field-long" value="<?php echo $dataCB['nom']; ?>" />
+					</td>
+					</tr>
+
+					<tr>
+					<td><label>Date d expiration <span class="required"></span></label></td>
+					<td><input type="text" name="dateExp" class="field-long" value="<?php echo $dataCB['dateExp']; ?>" />
+					</td>
+					</tr>
+
+					<tr>
+					<td><label>Code de securite <span class="required"></span></label></td>
+					<td><input type="text" name="code_secu" class="field-long" value="<?php echo $dataCB['code_secu']; ?>" />
+					</td>
+					</tr>
+
+				<tr>
+					<td colspan="2" align="center">
+						<input type="submit" name="acheter" value="Acheter">
+					</td>
+
+				</tr>
+
+			</table>
+		</form>
+
+		</div>
+
+		<div id="footer">
+ 		<!--Copyright &copy; 2021 Prime Properties<br> -->
+
+ 		<dd>
+ 			<ul>
+ 				<li>
+ 					<p align=left>
+ 						Qui sommes-nous ?<br><br>
+ 						Nous sommes un groupe d'étudiants qui avons<br> pour but de créer un site internet pour vous permettre de<br> faire votre shopping facilement.<br>
+ 						Nous avons réfléchi à ce dont vous pourriez avoir<br> besoin et il nous est apparu que nous avons beaucoup <br>de difficultés à faire hanter les personnes qui le méritent...<br>
+ 						<p align=right>
+ 							Alors nous voila !
+ 						</p>
+ 					</p>
+ 				</li>
+ 				<li>
+ 					<p class="contact">Nous contacter: <br><br>
+ 						<a href="mailto:paris.shopping@gmail.com">paris.shopping@gmail.com</a><br>
+ 						67 avenue Henri Martin 75016 PARIS<br>
+ 						02 37 60 03 10<br>
+
+ 					</p>
+
+ 				</li>
+ 				<li>
+ 					<p>
+ 						Instagram des auteurs :<br>
+ 						<br> <a href="https://www.instagram.com/anaisline_/" class="button">> anaisline_</a>
+ 						<br> <a href="https://www.instagram.com/marine_rhd/" class="button">> marine_rhd</a>
+ 						<br> <a href="https://www.instagram.com/benji.lvld/" class="button">> benji.lvld</a>
+ 					</p>
+ 				</li>
+ 			</ul>
+ 		</dd>
+ 	</div>
+	</div>
+
+</body>
+</html>
---
+++ achatImmediat_2.php
@@ -0,0 +1,150 @@
+<?php
+session_start();
+$id_acheteur=$_SESSION['id_acheteur'];
+?>
+
+<?php
+
+$database = "shopping";
+//connectez-vous dans BDD
+$db_handle = mysqli_connect('localhost', 'root', '');
+$db_found = mysqli_select_db($db_handle, $database);
+
+$nom=isset($_POST['nom'])?$_POST['nom']:"";
+	$prenom=isset($_POST['prenom'])?$_POST['prenom']:"";
+	$adresse_l1=isset($_POST['adresse_l1'])?$_POST['adresse_l1']:"";
+	$adresse_l2=isset($_POST['adresse_l2'])?$_POST['adresse_l2']:"";
+	$ville=isset($_POST['ville'])?$_POST['ville']:"";
+	$code_postal=isset($_POST['code_postal'])?$_POST['code_postal']:"";
+	$tel=isset($_POST['tel'])?$_POST['tel']:"";
+	$type=isset($_POST['type'])?$_POST['type']:"";
+	$numero_cb=isset($_POST['numero_cb'])?$_POST['numero_cb']:"";
+	$nomCarte=isset($_POST['nomCarte'])?$_POST['nomCarte']:"";
+	$dateExp=isset($_POST['dateExp'])?$_POST['dateExp']:"";
+	$code_secu=isset($_POST['code_secu'])?$_POST['code_secu']:"";
+
+if (isset($_POST["acheter"])) {
+
+
+	if ($db_found) {
+
+	//Vérifier que tous les champs sont bien remplis. Dans le cas contraire, afficher un message d’erreur indiquant quel champ est vide.//blindage
+	$erreur = "";
+	if ($nom == "") {
+		$erreur .= "Le champ nom est vide. <br>";
+	}
+	if ($prenom == "") {
+		$erreur .= "Le champ prenom est vide. <br>";
+	}
+	if ($adresse_l1 == "") {
+		$erreur .= "Le champ adresse 1 est vide. <br>";
+	}
+	if ($ville == "") {
+		$erreur .= "Le champ ville est vide. <br>";
+	}
+	if ($code_postal == "") {
+		$erreur .= "Le champ code postal est vide. <br>";
+	}
+	if ($tel == "") {
+		$erreur .= "Le champ telephone est vide. <br>";
+	}
+	if ($type == "") {
+		$erreur .= "Le champ type de carte est vide. <br>";
+	}
+	if ($numero_cb == "") {
+		$erreur .= "Le champ numero de carte est vide. <br>";
+	}
+	if ($nomCarte == "") {
+		$erreur .= "Le champ nom de la carte est vide. <br>";
+	}
+	if ($dateExp == "") {
+		$erreur .= "Le champ date d expiration est vide. <br>";
+	}
+	if ($code_secu == "") {
+		$erreur .= "Le champ code de securite est vide. <br>";
+	}
+	if ($erreur == "") {
+		echo "Toutes les informations sont rentrees";//a voir pour remplacer
+
+		if($nom!="")
+        {
+            $sql="UPDATE acheteur SET nom='$nom' WHERE id_acheteur='$id_acheteur' ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($prenom!="")
+        {
+            $sql="UPDATE acheteur SET prenom='$prenom' WHERE id_acheteur='$id_acheteur' ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($adresse_l1!="")
+        {
+            $sql="UPDATE adresse SET adresse_l1='$adresse_l1' WHERE id_adresse=(select id_adresse from acheteur where id_acheteur='$id_acheteur') ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($adresse_l2!="")
+        {
+            $sql="UPDATE adresse SET adresse_l2='$adresse_l2' WHERE id_adresse=(select id_adresse from acheteur where id_acheteur='$id_acheteur') ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($ville!="")
+        {
+            $sql="UPDATE adresse SET ville='$ville' WHERE id_adresse=(select id_adresse from acheteur where id_acheteur='$id_acheteur') ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($code_postal!="")
+        {
+            $sql="UPDATE adresse SET code_postal='$code_postal' WHERE id_adresse=(select id_adresse from acheteur where id_acheteur='$id_acheteur') ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($tel!="")
+        {
+            $sql="UPDATE acheteur SET tel='$tel' WHERE id_acheteur='$id_acheteur' ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($type!="")
+        {
+            $sql="UPDATE cb SET type='$type' WHERE id_acheteur='$id_acheteur' ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($numero_cb!="")
+        {
+            $sql="UPDATE cb SET numero_cb='$numero_cb' WHERE id_acheteur='$id_acheteur' ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($nomCarte!="")
+        {
+            $sql="UPDATE cb SET nom='$nomCarte' WHERE id_acheteur='$id_acheteur' ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($dateExp!="")
+        {
+            $sql="UPDATE cb SET dateExp='$dateExp' WHERE id_acheteur='$id_acheteur' ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+        if($code_secu!="")
+        {
+            $sql="UPDATE cb SET code_secu='$code_secu' WHERE id_acheteur='$id_acheteur' ";
+            $result = mysqli_query($db_handle,$sql);
+        }
+
+
+
+
+
+
+	}
+	else
+	{
+		echo "erreur: " .$erreur;
+	}
+
+	}
+	else
+	{
+		echo "pb de connexion";
+	}
+
+
+}
+
+?>
